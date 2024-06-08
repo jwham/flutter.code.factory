0cc4df4e31fee5a5b890b05860630cf08a3a9a41
jwham <jwham@checkupv.com>
2024-06-08 22:52:25 +0900

    flutter study

--- ch01/dart_lang/lib/1.3/5_dynamic_keyword.dart
+++ ch01/dart_lang/lib/1.3/5_dynamic_keyword.dart
@@ -1,4 +1,9 @@
 void main() {
-  dynamic name = '코드팩토리';
-  name = 1;
+  // var 타입은 변수의 값을 사용해서 변수의 타입을 유추하는 키워드다.
+  // 타입을 한 번 유추하면 추론된 타입값이 고정된다.
+  // 하지만 dynamic 을 사용하면 변수 타입이 고정되지 않는다.
+  dynamic name = '코드팩토리'; //문자열에서
+  print(name);
+  name = 1; // 숫자로
+  print(name);
 }
